2637f57730dc51ab7eb290eff8821b49f7d8d274
kulyaginv <kulyaginv@gmail.com>
2020-04-26 16:16:25 +0300

    20200426-1616

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -78,14 +78,20 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': 'workout',
+#        'USER': 'postgres',
+#        'PASSWORD': 'password',
+#        'HOST': '192.168.100.1',
+#        'PORT': '5432',
+#    }
+#}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'workout',
-        'USER': 'postgres',
-        'PASSWORD': 'password',
-        'HOST': '192.168.100.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'mydatabase',
     }
 }
 
