50d43215228641262c218ae6a5a7582cd8454a47
kulyaginv <kulyaginv@gmail.com>
2020-04-28 15:02:11 +0300

    Chart

--- mysite/charts/urls.py
+++ mysite/charts/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name='charts'
 urlpatterns = [
     path('', views.chart01, name='chart01'),
+    path('detail/<int:pk>', views.chart02, name='chart02'),
     #path('post/', views.workoutadd, name='post'),
     #path('<int:pk>/', views.WorkoutDetailView.as_view(), name='detail'),
     #path('edit/<int:pk>/', views.edit, name='edit'),
--- mysite/charts/views.py
+++ mysite/charts/views.py
@@ -1,6 +1,17 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from exercise.models import Exercise
+from workouts.models import Workouts
 
 @login_required(login_url='/login')
 def chart01(request):    
-    return render(request, 'charts/chart01.html')
+    exercises = Exercise.objects.filter(userid = request.user).filter(types='W')
+    return render(request, 'charts/chart01.html', {'dataset1' : exercises})
+
+@login_required(login_url='/login')
+def chart02(request, pk):
+    exercise = get_object_or_404(Exercise, pk=pk)
+    workouts = Workouts.objects.filter(exid = exercise.pk)
+    labels_list = [date.strftime("%Y-%m-%d") for date in workouts.values_list('time', flat=True)]
+    data_list = list(workouts.values_list('c1', flat = True))
+    return render(request, 'charts/chart02.html', {'exercise': exercise, 'labels_list' : labels_list, 'data_list' : data_list})
